0569059a50bdacbe4a94564a1c4e836bdd379582
Acmasule777 <atulmasule7@gmail.com>
2026-08-25 17:59:18 +0530

    this is the shared project that both sevices used like EmployeService and NotificationService this used for the data mapping

---
+++ Shared/EmployeeCreatedMessage.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared
+{
+    public class EmployeeCreatedMessage
+    {
+        public int EmployeeId { get; set; }
+
+        public string EmployeeName { get; set; } = string.Empty;
+
+        public string EmployeeEmail { get; set; } = string.Empty;
+    }
+}

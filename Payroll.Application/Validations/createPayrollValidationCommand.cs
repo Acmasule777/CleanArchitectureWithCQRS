6c6ad1b60e4d2ed159d60541f226eb306573629c
Acmasule777 <atulmasule7@gmail.com>
2026-09-02 14:50:48 +0530

    I fixed that test case is that is failed with ci.yml actions from github actions

--- Payroll.Application/Validations/createPayrollValidationCommand.cs
+++ Payroll.Application/Validations/createPayrollValidationCommand.cs
@@ -13,7 +13,7 @@ namespace Payroll.Application.Validations
         public createPayrollValidationCommand()
         {
             RuleFor(p => p.dto.Allowance).GreaterThan(0).WithMessage("Allowance should be greaterthan zero and even not equal to zero");
-            //RuleFor(p => p.dto.Deduction).LessThan(500).WithMessage("Deduction should be lessthan 500");
+            RuleFor(p => p.dto.Deduction).LessThan(500).WithMessage("Deduction should be lessthan 500");
             RuleFor(p => p.dto.BasicSalary).GreaterThan(0).NotEmpty().WithMessage("Basic salary should be greater that zero and even not equal to zero");
             RuleFor(p => p.dto.PayrollMonth).NotEmpty().WithMessage("PayrollMonth should not empty");
             RuleFor(p => p.dto.NetSalary).GreaterThan(0).NotNull().WithMessage("NetSalary should be greater that zero and even not equal to zero");
